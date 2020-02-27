ab388a139309a49b54f0b7ffaeb39b0fa4cbdef6
Sean Birmingham <seannbirmingham98@gmail.com>
2020-02-26 20:56:32 -0500

    set up ability to create a jwt when adding a user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -47,6 +49,24 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
