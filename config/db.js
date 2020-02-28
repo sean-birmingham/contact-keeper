209536b010bc99ed473c87c2474b023197a4bed5
Sean Birmingham <seannbirmingham98@gmail.com>
2020-02-27 20:39:58 -0500

    fixed problems with db.js, set up auth and middleware for logging in

--- config/db.js
+++ config/db.js
@@ -7,11 +7,14 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      useUnifiedTopology: true
     });
+
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
+    // Exit process with failure
     process.exit(1);
   }
 };
