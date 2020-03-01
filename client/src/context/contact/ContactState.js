da2f9ad5d497ce396078e60ad7fd29d8504f418f
Sean Birmingham <seannbirmingham98@gmail.com>
2020-03-01 10:27:41 -0500

    set up functionality to update contact, added clear button to clear the form

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -64,6 +64,9 @@ const ContactState = props => {
   };
 
   // Update Current Contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // Filter Contacts
 
@@ -77,7 +80,8 @@ const ContactState = props => {
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}>
       {props.children}
     </ContactContext.Provider>
