26911692d36837bbc7f252df1373c8962a725bd4
Sean Birmingham <seannbirmingham98@gmail.com>
2020-03-03 18:28:05 -0500

    set up functionality to logout

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -94,6 +94,9 @@ const AuthState = props => {
   };
 
   // Logout
+  const logout = () => {
+    dispatch({ type: LOGOUT });
+  };
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
@@ -108,6 +111,7 @@ const AuthState = props => {
         error: state.error,
         register,
         login,
+        logout,
         clearErrors,
         loadUser
       }}>
